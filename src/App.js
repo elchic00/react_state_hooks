ce68425907a7ae980db1c42e354c54c73dced25a
Andrew Alagna <aalagna04@gmail.com>
2022-03-23 19:35:03 -0400

    push react app for gh pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './components/Clock';
+import Content from './components/Content';
+
+function App() {
+  return (
+    <>
+    <Content/>
+    </>
+    );
+}
+
+export default App;
